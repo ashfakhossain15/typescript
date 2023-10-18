e3750346be18df9f0f7041d6904ccaae1594e16d
Ashfak Hossain <ashfaksikder15@gmail.com>
2023-10-18 21:41:00 +0600

    array data type

---
+++ user-define-type/array-type.js
@@ -0,0 +1,9 @@
+// let userNames: string[];
+var userNames;
+var userId;
+var isUser;
+var multipleTypes;
+multipleTypes = [575125, 'number'];
+console.log(multipleTypes);
+userNames = ["fahim", "tuhin", "sohan"];
+console.log(userNames.sort());
---
+++ user-define-type/array-type.ts
@@ -0,0 +1,12 @@
+// let userNames: string[];
+let userNames: Array<string>;
+let userId: number[];
+let isUser: boolean[];
+
+let multipleTypes: (string | number)[];
+multipleTypes = [575125, 'number']
+console.log(multipleTypes)
+
+userNames = ["fahim","tuhin" ,"sohan"]
+
+console.log(userNames.sort());
