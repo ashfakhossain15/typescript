672ec6677665d5d9f8af7b2a8692c9eb81579ac5
Ashfak Hossain <ashfaksikder15@gmail.com>
2023-10-18 12:55:51 +0600

    basic type of typescript

---
+++ index2.js
@@ -0,0 +1,15 @@
+var firstName;
+var lastName;
+var fullName;
+var userId;
+var isActivated;
+firstName = "Ashfak";
+lastName = "Hossain";
+userId = 575125;
+isActivated = true;
+fullName = firstName.concat(lastName);
+console.log("Your User ID is ".concat(userId, " and userName is ").concat(fullName, "\naccount is").concat(isActivated));
+console.log(fullName.split(" "));
+console.log(fullName.split(""));
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
---
+++ index2.ts
@@ -0,0 +1,19 @@
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let userId: number;
+let isActivated: boolean;
+
+
+firstName = "Ashfak";
+lastName = "Hossain";
+userId = 575125;
+isActivated = true;
+fullName = firstName.concat(lastName)
+
+console.log(`Your User ID is ${userId} and userName is ${fullName}
+account is${isActivated}`);
+console.log(fullName.split(" "));
+console.log(fullName.split(""));
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
