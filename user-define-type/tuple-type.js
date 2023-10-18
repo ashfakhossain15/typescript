0582ab682046b53509af321027ca4b0656da16ca
Ashfak Hossain <ashfaksikder15@gmail.com>
2023-10-18 22:25:55 +0600

    tuple data type

---
+++ user-define-type/tuple-type.js
@@ -0,0 +1,4 @@
+var user;
+user = [101, 'username'];
+user.push("102", 'password');
+console.log(user[0]);
---
+++ user-define-type/tuple-type.ts
@@ -0,0 +1,7 @@
+let user: [number, string]
+user = [101, 'username']
+
+user.push("102", 'password')
+
+console.log(user[0 ]);
+ 
