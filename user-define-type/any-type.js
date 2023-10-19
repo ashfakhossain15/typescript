dfb57f4eb87da5318328a0efecc9d62f7b19a6b6
Ashfak Hossain <ashfaksikder15@gmail.com>
2023-10-19 22:42:13 +0600

    any type

---
+++ user-define-type/any-type.js
@@ -0,0 +1,6 @@
+var user;
+user = "ashfak";
+user = 575125;
+user = true;
+user = ["user1", "user2", "user3"];
+console.log(user);
---
+++ user-define-type/any-type.ts
@@ -0,0 +1,7 @@
+let user: any;
+user = "ashfak";
+user = 575125;
+user = true;
+user = ["user1", "user2", "user3"];
+
+console.log(user);
